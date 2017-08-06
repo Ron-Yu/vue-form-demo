a196d3ec594d773f9a89b0035c146d68821994f8
Ron Yu <ronyu77@gmail.com>
2017-08-07 00:03:15 +0800

    feat(component): add MyRadio component

---
+++ src/components/fields/MyRadio.vue
@@ -0,0 +1,46 @@
+<template>
+  <p>
+    <input
+      type="radio"
+      class="with-gap"
+      :name="fieldName" 
+      :id="label"
+      :value="value"
+      @change="updateValue($event.target.value)"
+      />
+    <label :for="label">{{label}}</label>
+  </p>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    value: String,
+    fieldName: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    rules: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('change', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
