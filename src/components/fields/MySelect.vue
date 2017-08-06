38ee59418d946609038748fe703d248ee4d32f64
Ron Yu <ronyu77@gmail.com>
2017-08-07 00:03:43 +0800

    feat(component): add MySelect component

---
+++ src/components/fields/MySelect.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+      <label>{{label}}</label>
+      <select @change="updateValue($event.target.value)" class="browser-default">
+        <option value="" disabled selected>{{label}}</option>
+        <template v-for="option in options">
+          <option :value="option">{{option}}</option>
+        </template>
+      </select>
+  </section>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    options: {
+      type: Array,
+      default: []
+    },
+    label: {
+      type: String,
+      default: 'select'
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('change', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
