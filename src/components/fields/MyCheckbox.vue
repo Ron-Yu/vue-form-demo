eeb0aa97a69001bd50e5ff578de0cc12557d1900
Ron Yu <ronyu77@gmail.com>
2017-08-07 00:03:29 +0800

    feat(component): add MyCheckbox component

---
+++ src/components/fields/MyCheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <p>
+    <input
+      type="checkbox"
+      class="filled-in"
+      :name="fieldName"
+      :id="label"
+      :value="value"
+      @change="updateValue($event.target.value)"
+      />
+    <label :for="label">{{label}}</label>
+  </p>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean,
+    value: String,
+    fieldName: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('change', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
