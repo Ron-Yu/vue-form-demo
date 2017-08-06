3dec9eb6b68e676e40116cb08eb994dd169a66a7
Ron Yu <ronyu77@gmail.com>
2017-08-07 00:03:53 +0800

    feat(component): add MyButton component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="btn waves-effect waves-light myButton"
+    @click.prevent="handleValidate"
+    >
+    {{text}}
+    <i class="material-icons right">send</i>
+  </button>
+</template>
+
+<script>
+const INI_PROPS = {
+  text: 'submit',
+  handleValidate: () => {}
+}
+
+export default {
+  props: {
+    text: {
+      type: String,
+      default: INI_PROPS.text
+    },
+    handleValidate: {
+      type: Function,
+      default: INI_PROPS.handleValidate
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
